2bfdc0d4f9bd63426630e00c6467b09e2a98c6b9
lopezolidev <olivera9806@gmail.com>
2025-10-23 18:21:04 -0400

    feat: Implement shipping type usage and product sales contribution reports
    -  Query A: Generates a detailed report on shipping types, calculating usage frequency, revenue generated, and their respective proportions/percentages to support simplification decisions.
    
    - Query I: Identifies the top 10 best-selling products and calculates their percentage contribution to total revenue for key performance analysis.

--- sql/03_queries.sql
+++ sql/03_queries.sql
@@ -19,3 +19,33 @@ JOIN
     Inventario AS i ON p.id = i.productoId
 WHERE 
     c.nombre = 'Snacks' ;
+
+-- Query A: Delivery types report and analytics. Tables used: OrdenOnline, TipoEnvio.
+SELECT
+    te.nombreEnvio AS Nombre_Envio ,
+    COUNT(*) AS cantidad_veces_usado , 
+    ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER(), 2)  AS Proporcion_respecto_total ,
+    SUM(te.costoEnvio) AS Ingresos_totales_tipo_envio ,
+    ROUND(SUM(te.costoEnvio) * 100.0 / SUM(SUM(te.costoEnvio)) OVER(), 2) AS Proporcion_ingreso_respecto_total
+FROM 
+    OrdenOnline AS oo
+    JOIN TipoEnvio AS te ON te.id = oo.tipoEnvioId
+GROUP BY
+    te.nombreEnvio ;
+
+-- Query I: Top 10 sold products
+SELECT
+    p.nombre ,
+    ROUND(SUM(fd.cantidad * fd.precioPor) * 100.0 / SUM(SUM(fd.cantidad * fd.precioPor)) OVER(), 2) 
+        AS Contribucion_precio_%
+FROM 
+    FacturaDetalle AS fd
+    JOIN Producto AS p ON p.id = fd.productoId
+GROUP BY
+    fd.productoId, p.nombre
+ORDER BY
+    Contribucion_precio DESC
+LIMIT
+    10 ;
+
+-- Query B: 
